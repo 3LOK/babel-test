f3ee3734b468668bbc63daaa0a73e30387390e94
3LOK <yoava@wix.com>
2016-09-29 17:35:16 +0300

    minimal example

--- spec/setup.js
+++ spec/setup.js
@@ -16,16 +16,7 @@ function initBabelRegister() {
 function initJsdom() {
     var jsdom = require('jsdom').jsdom;
 
-    var exposedProperties = ['window', 'navigator', 'document'];
-
     global.document = jsdom('');
     global.window = document.defaultView;
-
-    Object.keys(document.defaultView).forEach((property) => {
-        if (typeof global[property] === 'undefined') {
-            exposedProperties.push(property);
-            global[property] =
-                document.defaultView[property];
-        }
-    });
+    global['XMLHttpRequest'] = document.defaultView['XMLHttpRequest'];
 }
