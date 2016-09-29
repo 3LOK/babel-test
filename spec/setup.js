58d217f84ac37037c88cd5769a69395556f45883
3LOK <yoava@wix.com>
2016-09-29 15:52:47 +0300

    better test

---
+++ spec/setup.js
@@ -0,0 +1,31 @@
+initBabelRegister();
+initJsdom();
+
+function initBabelRegister() {
+    require("babel-register")({
+        ignore: function(filename) {
+            if (filename.indexOf("node_modules") !== -1)
+                return true;
+            return false;
+        },
+        "presets": ["react", "es2015", "stage-2"]
+    });
+}
+
+//required for node testing.
+function initJsdom() {
+    var jsdom = require('jsdom').jsdom;
+
+    var exposedProperties = ['window', 'navigator', 'document'];
+
+    global.document = jsdom('');
+    global.window = document.defaultView;
+
+    Object.keys(document.defaultView).forEach((property) => {
+        if (typeof global[property] === 'undefined') {
+            exposedProperties.push(property);
+            global[property] =
+                document.defaultView[property];
+        }
+    });
+}
---
+++ spec/shared/I18nify/index.spec.js
@@ -0,0 +1,9 @@
+import { mount } from 'enzyme';
+import { expect } from 'chai';
+import _ from 'lodash';
+import React from 'react';
+
+describe('Dummy', () => {
+    it('blah', () => {
+    });
+});
